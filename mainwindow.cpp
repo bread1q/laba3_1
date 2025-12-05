f0a40d7c3b4d5397ad5b1a8cf85927610f8f397d
bread1q <osnova326@gmail.com>
2025-12-05 16:45:38 +0500

    Добавлены методы класса CCircle

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QPainter>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +12,26 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+CCircle::CCircle() : x_(x), y_(y) {}
+
+bool CCircle::contains(int x, int y) const {
+    int dx = x - x_;
+    int dy = y - y_;
+
+    return (dx * dx + dy * dy) <= (radius_ * radius_);
+}
+
+void CCircle::draw(QPainter &painter) const {
+    if (selected_) {
+        painter.setPen(QPen(Qt::red, 2));
+        painter.setBrush(QBrush(QColor(255, 200, 200, 150))); // светло-красный
+    }
+    else {
+        painter.setPen(QPen(Qt::blue, 1));
+        painter.setBrush(QBrush(QColor(200, 200, 255, 150))); // светло-синий
+    }
+
+    painter.drawEllipse(x_ - radius_, y_ - radius_, 2 * radius_, 2 * radius_);
 }
